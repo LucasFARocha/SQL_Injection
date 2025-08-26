4c32ef7844a6c6b1581aed49c69489a8d59ccde9
Lucas Rocha <lucasfarocha44@gmail.com>
2025-08-26 15:58:24 -0300

    first commit

---
+++ conecta.php
@@ -0,0 +1,11 @@
+<?php
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "pentest";
+    $conexao = mysqli_connect($servidor, $usuario, $senha,$banco );
+    
+    if (!$conexao) {
+    die("Connection failed: " . mysqli_connect_error());
+    }else{ echo "conectado";}
+?>
